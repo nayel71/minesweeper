7227b40fe79d4f49253fe1d64eef4b5a54bc8088
Samin Riasat <nayel@Samins-MacBook-Air.local>
2019-10-28 02:21:04 -0400

    created separate terminal for cli version

--- README.md
+++ README.md
@@ -6,13 +6,26 @@ Download the repository, open it in terminal and run `make`. To play, enter
 
 `src/main [gui|cli] [width] [height] [mine count]`
 
+# Pre-requisites
+
+- [GTK](https://www.gtk.org)
+- A terminal supporting ANSI escape codes
+
 # Features
 - The player chooses the grid dimensions and the number of mines.
 - (GUI version) Colour-coded button labels.
 - (GUI version) Left-clicking on a cell reveals it, right-clicking on a cell flags/unflags it.
-- (Command-line version) Whitespace is ignored when entering commands.
-- (Command-line version) At most one command may be entered at a time. (This helps prevent mistakes.)
-- (Command-line version) Entering an invalid command displays instructions. (They are also displayed at the start.)
+- (CLI version) Independent terminal.
+- (CLI version) Spaces and invalid commands are ignored.
+
+# CLI Screenshots
+![CLI Start](screenshots/cli_start.png)
+
+![CLI Play](screenshots/cli_play.png)
+
+![CLI Won](screenshots/cli_won.png)
+
+![CLI Lost](screenshots/cli_lost.png)
 
 # GUI Screenshots
 ![GUI Start](screenshots/gui_start.png)
--- cli/cli.c
+++ cli/cli.c
@@ -3,11 +3,14 @@
 #include <assert.h>
 #include "cli.h"
 
+static const char *erase_line = "\r\e[K";
+static const char *go_up = "\e[%dA";
+
 void print_board(const struct board *b) {
 	assert(b);
 	printf("___|");
 	for (int x = 1; x <= b->width; x++) {
-		printf("_%02d", x);
+		printf(x < 10? "__%d" : "_%02d", x);
 	}
 	printf("\n");
 	for (int y = 1; y <= b->height; y++) {
@@ -20,48 +23,70 @@ void print_board(const struct board *b) {
 }
 
 void print_commands(void) {
-	printf("Enter commands in the form c x y, where\n");
-	printf("\tc is 'f' (flag) or 'r' (reveal),\n");
-	printf("\tx is the column identifier,\n");
-	printf("\ty is the row identifier.\n");
-	printf("Enter 'q' to quit.\n");
+	printf("Enter commands as f(lag)/r(eveal) [col] [row]. Enter q(uit) to quit.\n");
+	printf("command> ");
+}
+
+static inline int is_digit(char c) { return '0' <= c && c <= '9'; }
+
+static inline void ignore_spaces(char *line) { while (*line == ' ') line++; }
+
+// reads two space-separated ints from line and stores them at x and y
+static void read_two_ints(char *line, int *x, int *y) {
+	while (line && !is_digit(*line)) { // ignore non-digit characters
+		line++;
+	}
+	*x = atoi(line);
+	while (line && is_digit(*line)) { // ignore the digits of *x
+		line++;
+	}
+	ignore_spaces(line);
+	*y = atoi(line);
 }
 
 int play_cli(struct board *b) {
 	print_board(b);
 	print_commands();
 
-	// continuously read commands
-	char command;
+	// continuously read and execute commands
+	char *line = NULL;
+	size_t n = 0;
 	int x, y;
-	while (scanf(" %c", &command)) {
+	while (getline(&line, &n, stdin)) {
+		ignore_spaces(line);
+		char command = *line;
 		if (command == 'f') {
-			scanf("%d%d", &x, &y);
+			read_two_ints(line, &x, &y);
 			if (flag(b, x, y)) {
+				printf(go_up, b->height + 3);
 				print_board(b);
-	  		} else {
 				print_commands();
-	  		}
+			} else {
+				printf(go_up, 1);
+			}
 		} else if (command == 'r') {
-			scanf("%d%d", &x, &y);
+			read_two_ints(line, &x, &y);
 			if (reveal(b, x, y)) {
+				printf(go_up, b->height + 3);
 				print_board(b);
+				print_commands();
 				if (game_won(b)) {
-					printf("Well done!\n");
+					printf("%sWell Done\n", erase_line);
 					return EXIT_SUCCESS;
 				} else if (game_lost(b)) {
-					printf("Game Over\n");
+					printf("%sGame Over\n", erase_line);
 					return EXIT_SUCCESS;
 				}
-	  		} else {
-				print_commands();
-	  		}
+			} else {
+				printf(go_up, 1);
+			}
 		} else if (command == 'q') {
 			return EXIT_SUCCESS;
 		} else {
-			print_commands();
+			printf(go_up, 1);
 		}
-		while (getchar() != '\n');
+		line = NULL;
+		printf("%scommand> ", erase_line);
 	}
 
 	return EXIT_FAILURE;
