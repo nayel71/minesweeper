111390c295a59ccaf47a51c5512f5a0cca85bb34
Samin Riasat <nayel71@gmail.com>
2020-10-08 23:02:01 -0400

    Updates

---
+++ precache-manifest.b26df903006caebfc8057690a751cec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4d9903011a055b2ba10d96ebd7e212",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "cfe342570ce2929153b9",
+    "url": "/minesweeper/static/css/main.d11919c5.chunk.css"
+  },
+  {
+    "revision": "4453cf9469d7ea9c8b7a",
+    "url": "/minesweeper/static/js/2.a0aeb42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.a0aeb42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe342570ce2929153b9",
+    "url": "/minesweeper/static/js/main.6609c9fc.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
