2610463f2b90914a7c51850eed09ca94bbddb8af
Samin Riasat <nayel71@gmail.com>
2020-10-16 08:31:57 -0400

    replaced tab with spaces in js files

--- JavaScript/src/App.test.js
+++ JavaScript/src/App.test.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
-	const div = document.createElement("div");
-	ReactDOM.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		div
-	);
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    div
+  );
 });
--- JavaScript/src/Minesweeper.test.js
+++ JavaScript/src/Minesweeper.test.js
@@ -2,41 +2,41 @@ import Minesweeper from "./Minesweeper";
 jest.mock("./Minesweeper");
 
 beforeEach(() => {
-	Minesweeper.mockClear();
+  Minesweeper.mockClear();
 });
 
 it("class constructed", () => {
-	const game = new Minesweeper();
-	expect(Minesweeper).toHaveBeenCalled();
+  const game = new Minesweeper();
+  expect(Minesweeper).toHaveBeenCalled();
 });
 
 it("methods called", () => {
-	expect(Minesweeper).not.toHaveBeenCalled();
+  expect(Minesweeper).not.toHaveBeenCalled();
 
-	const game = new Minesweeper();
-	const mockGame = Minesweeper.mock.instances[0];
+  const game = new Minesweeper();
+  const mockGame = Minesweeper.mock.instances[0];
 
-	game.createButtons();
-	const mockCreateButtons = mockGame.createButtons;
-	expect(mockCreateButtons).toHaveBeenCalledTimes(1);
+  game.createButtons();
+  const mockCreateButtons = mockGame.createButtons;
+  expect(mockCreateButtons).toHaveBeenCalledTimes(1);
 
-	game.updateButtons();
-	const mockUpdateButtons = mockGame.updateButtons;
-	expect(mockUpdateButtons).toHaveBeenCalledTimes(1);
+  game.updateButtons();
+  const mockUpdateButtons = mockGame.updateButtons;
+  expect(mockUpdateButtons).toHaveBeenCalledTimes(1);
 
-	game.handleContextMenu();
-	const mockHandleContextMenu = mockGame.handleContextMenu;
-	expect(mockHandleContextMenu).toHaveBeenCalledTimes(1);
+  game.handleContextMenu();
+  const mockHandleContextMenu = mockGame.handleContextMenu;
+  expect(mockHandleContextMenu).toHaveBeenCalledTimes(1);
 
-	game.generateMines();
-	const mockGenerateMines = mockGame.generateMines;
-	expect(mockGenerateMines).toHaveBeenCalledTimes(1);
+  game.generateMines();
+  const mockGenerateMines = mockGame.generateMines;
+  expect(mockGenerateMines).toHaveBeenCalledTimes(1);
 
-	game.flag();
-	const mockFlag = mockGame.flag;
-	expect(mockFlag).toHaveBeenCalledTimes(1);
+  game.flag();
+  const mockFlag = mockGame.flag;
+  expect(mockFlag).toHaveBeenCalledTimes(1);
 
-	game.reveal();
-	const mockReveal = mockGame.reveal;
-	expect(mockReveal).toHaveBeenCalledTimes(1);
+  game.reveal();
+  const mockReveal = mockGame.reveal;
+  expect(mockReveal).toHaveBeenCalledTimes(1);
 });
